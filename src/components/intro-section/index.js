a29d0fef72ade8fd8ba5120eda01a74ddce05f9b
sjchae1207 <sjchea@kaist.ac.kr>
2023-03-20 14:34:18 +0900

    minor changes

--- src/components/intro-section/index.js
+++ src/components/intro-section/index.js
@@ -3,17 +3,18 @@ import SectionHeader from '../section-header';
 import IconButtonBar from '../icon-button-bar';
 import './style.scss';
 
+
 function IntroSection({intros}) {
   if (!intros) return null; 
   return (
     <div className="intro-section">
-    {intros.map((intro, index) =>
-      index === 0 ? null : (
-        <div className="intro" key={index}>
-            <div className="description">{intro.description}</div>
-        </div>
-      ),
-    )}
+      {intros.map((intro, index) =>
+        index === 0 ? null : (
+          <div className="intro" key={index}>
+              <div className="description">{intro.description}</div>
+          </div>
+        ),
+      )}
     </div>
   );
 }
